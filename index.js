e8cd1e58a8bfb51147ab142a836d1c0374c0b6d0
mohtashim haider <rohithaider@gmail.com>
2024-12-19 16:09:14 +0600

    getting data from server /jobs

--- index.js
+++ index.js
@@ -29,6 +29,17 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+    
+    const jobCollection = client.db("Job-portal").collection("jobs");
+
+    app.get('/jobs',async(req,res)=>{
+        const cursor = jobCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
